aa17ccd0cf67b63a309543b2d764378ed2be9d28
DESKTOP-U34IE0R\EL <latif.abdurrahman@ugm.mail.co.id>
2017-03-18 20:01:06 +0700

    ganti lagi

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,75 +2,7 @@
 
 @section('content')
 
-<body>
-    <!-- Loader-->
-    <div id="page-preloader"><span class="spinner"></span></div>
-    <!-- Loader end-->
-    <div data-header="sticky" data-header-top="200" class="layout-theme animated-css">
-      <!-- Start Switcher-->
-      <div class="switcher-wrapper">
-        <div class="demo_changer">
-          <div class="demo-icon color_primary"><i class="fa fa-cog fa-spin fa-2x"></i></div>
-          <div class="form_holder">
-            <div class="row">
-              <div class="col-xs-12">
-                <div class="predefined_styles">
-                  <div class="skin-theme-switcher">
-                    <h4>Color</h4><a href="javascript:void(0);" data-switchcolor="color1" style="background-color:#df0001;" class="styleswitch"></a><a href="javascript:void(0);" data-switchcolor="color2" style="background-color:#86a800;" class="styleswitch"></a><a href="javascript:void(0);" data-switchcolor="color3" style="background-color:#00adb4;" class="styleswitch"></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- end switcher-->
-      <div class="cd-main">
-        <div class="wrap-content cd-section cd-selected">
-          <header class="header">
-            <!-- <div class="top-header">
-              <div class="container">
-                <div class="row">
-                  <div class="col-xs-12">
-                    <div class="top-header__link bg-primary">
-                      <div class="top-header__wrap-link">top headlines<i class="icon fa fa-caret-right"></i></div>
-                    </div>
-                    <div class="top-header__ticker">
-                      <ul id="myUl">
-                        <li class="news-item"><a href="news_details-1.html" class="news-item__link">Uber offers $29m 'safe ride' settlement</a><i class="icon fa fa-adjust"></i></li>
-                        <li class="news-item"><a href="news_details-1.html" class="news-item__link">The robot that camouflages itself</a><i class="icon fa fa-adjust"></i></li>
-                        <li class="news-item"><a href="news_details-1.html" class="news-item__link">Luxury cars at the Detroit auto show</a><i class="icon fa fa-adjust"></i></li>
-                        <li class="news-item"><a href="news_details-1.html" class="news-item__link">Uber offers $29m 'safe ride' settlement</a><i class="icon fa fa-adjust"></i></li>
-                        <li class="news-item"><a href="news_details-1.html" class="news-item__link">The robot that camouflages itself</a><i class="icon fa fa-adjust"></i></li>
-                        <li class="news-item"><a href="news_details-1.html" class="news-item__link">Luxury cars at the Detroit auto show</a><i class="icon fa fa-adjust"></i></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div> -->
-            <div class="header-main">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-4">
-                    <ul class="social-links list-inline">
-                      <li><a href="twitter.com" class="social-links__item_link"><i class="icon fa fa-twitter"></i></a></li>
-                      <li><a href="facebook.com" class="social-links__item_link"><i class="icon fa fa-facebook"></i></a></li>
-                      <li><a href="instagram.com" class="social-links__item_link"><i class="icon fa fa-instagram"></i></a></li>
-                      <li><a href="linkedin.com" class="social-links__item_link"><i class="icon fa fa-linkedin"></i></a></li>
-                      <li><a href="pinterest.com" class="social-links__item_link"><i class="icon fa fa-pinterest-p"></i></a></li>
-                      <li><a href="youtube.com" class="social-links__item_link"><i class="icon fa fa-youtube-play"></i></a></li>
-                      <li><a href="rss.com" class="social-links__item_link"><i class="icon fa fa-rss"></i></a></li>
-                    </ul>
-                  </div>
-                  <div class="col-md-4"><a href="home.html" class="logo"><img src="assets/media/general/logo_mod-a.png" alt="Logo" class="logo__img img-responsive center-block"/></a></div>
-                  <div class="col-md-4">
-                    <div class="header-main__links"><a href="home.html" class="header-main__links-item">Sign In</a> or<a href="home.html" class="header-main__links-item"> Register</a><a href="#fakelink" class="search-open"><i class="icon pe-7s-search"></i></a><a href="#cd-nav" class="trigger cd-nav-trigger"><i class="icon pe-7s-menu"></i></a></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </header>
+
           <div class="wrap-nav">
             <nav class="navbar yamm">
               <div id="navbar-collapse-1" class="navbar-collapse collapse">
